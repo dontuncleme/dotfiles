341c9f7551457d23d2812ed2f692d5e7530a47fe
Sergey Samsonov <1289823+dontuncleme@users.noreply.github.com>
2024-09-30 17:20:41 +0200

    add wezterm

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,43 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- my coolnight colorscheme
+config.colors = {
+    foreground = "#CBE0F0",
+    background = "#011423",
+    cursor_bg = "#47FF9C",
+    cursor_border = "#47FF9C",
+    cursor_fg = "#011423",
+    selection_bg = "#033259",
+    selection_fg = "#CBE0F0",
+    ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
+    brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+}
+
+config.font = wezterm.font("Fira Code")
+config.font_size = 13
+
+config.enable_tab_bar = false
+
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.75
+config.macos_window_background_blur = 10
+
+config.initial_rows = 40
+config.initial_cols = 150
+
+config.keys = {
+    -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+    {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
+    -- Make Option-Right equivalent to Alt-f; forward-word
+    {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
+}
+
+-- and finally, return the configuration to wezterm
+
+return config
