9058845f9b0bd2c0df559c8d1b890450ec1ebebd
Sergey Samsonov <1289823+dontuncleme@users.noreply.github.com>
2025-08-08 19:53:45 +0200

    feat: add update script for managing dotfiles and dependencies

---
+++ update.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Homebrew
+if command -v brew >/dev/null 2>&1; then
+    brew update
+    brew upgrade
+    brew bundle --file=./homebrew/Brewfile
+    brew cleanup
+fi
+
+# OhMyZsh
+if [ -f "$HOME/.dotfiles/zsh/.zshrc" ]; then
+    ln -sf $HOME/.dotfiles/zsh/.zshrc $HOME
+fi
+
+# Starship
+if command -v starship >/dev/null 2>&1; then
+    mkdir -p $HOME/.config
+    ln -sf $HOME/.dotfiles/starship/starship.toml $HOME/.config
+fi
+
+# WezTerm
+if command -v wezterm >/dev/null 2>&1; then
+    mkdir -p $HOME/.config/wezterm/colors
+    ln -sf $HOME/.dotfiles/wezterm/.wezterm.lua $HOME
+fi
+
+# Update Dracula theme for WezTerm
+cd $HOME/.config/wezterm/colors/
+curl -O https://raw.githubusercontent.com/dracula/wezterm/refs/heads/main/dracula.toml
+cd - > /dev/null
+
+# git config
+if [ -f "$HOME/.dotfiles/git/.gitconfig" ]; then
+    ln -sf $HOME/.dotfiles/git/.gitconfig $HOME
+fi
+
+# GitHub Copilot
+if [ -f "$HOME/.dotfiles/github/copilot-instructions.md" ]; then
+    mkdir -p $HOME/.github
+    ln -sf $HOME/.dotfiles/github/copilot-instructions.md $HOME/.github/copilot-instructions.md
+fi
+
+# Update dotfiles repository
+git pull origin $(git rev-parse --abbrev-ref HEAD)
